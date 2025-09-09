1d3719577b1c8c8525b6ebb6107a7de8be29cd4c
jinseungbok <92.bokjs@gmail.com>
2025-09-09 16:36:15 +0900

    [docs]:250909_pointShop error decoding

--- src/router/index.js
+++ src/router/index.js
@@ -6,7 +6,7 @@ import Home from '@/views/HomeView.vue'
 import Community from '@/views/community/CommunityView.vue';
 import ChallengeHome from '@/views/challenge/ChallengeHome.vue';
 import ChallengeList from '@/views/challenge/ChallengeList.vue';
-import PointShop from '@/views/pointshop/PointShop.vue';
+import PointShop from '@/components/pointshop/PointShop.vue';
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
