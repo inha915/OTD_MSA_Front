cf784517ce5a424819cd11bdbc03c3f672e8b698
d-kjh <dkjh9942@gmail.com>
2025-09-10 17:01:18 +0900

    헤더 푸터 css작업

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,6 @@
 import Layout from './views/layout/layout.vue';
 import { useRoute, useRouter } from 'vue-router';
 import { ref, watch, onMounted } from 'vue';
-
-
-
 </script>
 
 <template>
@@ -13,26 +10,27 @@ import { ref, watch, onMounted } from 'vue';
   </div>
 </template>
 
-
 <style>
 /* 브라우저 전체 영역 중앙 정렬 */
-html, body, #app {
+html,
+body,
+#app {
   height: 100%;
   margin: 0;
   display: flex;
   justify-content: center; /* 가로 중앙 */
-  align-items: center;     /* 세로 중앙 */
-  background: #ffffff;     /* (선택) 바깥 배경색 */
+  align-items: center; /* 세로 중앙 */
+  background: #ffffff; /* (선택) 바깥 배경색 */
   /* 폰트 설정하고 예비로 쓸것 지정해둔거 */
   font-family: 'Pretendard', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-  Roboto, 'Helvetica Neue', Arial, 'Noto Sans KR', sans-serif;
+    Roboto, 'Helvetica Neue', Arial, 'Noto Sans KR', sans-serif;
 }
 
 .layout {
-  width: 390px; /* 아이폰 12 기준 */ 
+  width: 390px; /* 아이폰 12 기준 */
   height: 805px;
-  background: #FAFAFA;
-  box-shadow: 0 0 10px rgba(0,0,0,0.1);
-  border-radius: 20px;
+  background: #fafafa;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  border-radius: 60px;
 }
 </style>
--- src/views/layout/layout.vue
+++ src/views/layout/layout.vue
@@ -1,43 +1,40 @@
 <script setup>
-import Breadcrumb from './Breadcrumb.vue'
-import Content from './Content.vue'
-import Footer from './Footer.vue'
+import Breadcrumb from './Breadcrumb.vue';
+import Content from './Content.vue';
+import Footer from './Footer.vue';
 </script>
 <template>
   <div class="app-layout">
     <div>
-      <Breadcrumb /> <!--  상단 내용 -->
+      <Breadcrumb />
+      <!--  상단 내용 -->
     </div>
     <div class="main_margin">
       <router-view />
     </div>
     <!-- <Content />    컨텐츠 내용 -->
-    <Footer class="nav-footer   " /> <!-- 하단 네비게이션 -->
+    <Footer class="nav-footer" />
+    <!-- 하단 네비게이션 -->
   </div>
 </template>
-  
 
-  
-  <style scoped>
-  .app-layout {
-    display: flex;
-    row-gap: 20px; /* 행 간격을 20px로 설정 */
-    flex-direction: column;
-    height: 805px;
-
+<style lang="scss" scoped>
+.app-layout {
+  display: flex;
+  row-gap: 20px; /* 행 간격을 20px로 설정 */
+  flex-direction: column;
+  height: 805px;
+  user-select: none;
+}
+/* 공통 화면 좌우 여백 */
+.main_margin {
+  margin: 0px 20px 0 20px;
+  overflow: scroll;
+  &::-webkit-scrollbar {
+    display: none;
   }
-  /* 공통 화면 좌우 여백 */
-  .main_margin
-  {
-    margin: 0px 20px 0 20px; 
-  }
-  .nav-footer
-  {
-    margin-top: auto;
-   
-  }
-
-
-
-  
-  </style>
+}
+.nav-footer {
+  margin-top: auto;
+}
+</style>
