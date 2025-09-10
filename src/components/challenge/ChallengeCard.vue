0fded3c25484e3fc7c3f578d8489ca3ebf5a8c95
aspynlyn <lyn.aspyn@gmail>
2025-09-10 11:57:30 +0900

    챌린지카드

--- src/components/challenge/ChallengeCard.vue
+++ src/components/challenge/ChallengeCard.vue
@@ -10,19 +10,44 @@ const props = defineProps({
 </script>
 
 <template>
-  <div>
-    <img :src="`http://localhost:8080/challenge/${props.image}`"  />
-    <!-- :alt="`${props.name}`" -->
-    <div>
-      <img src="/public/image/main/point.png" alt="point" class="point" />{{
-        `${props.reward}P`
-      }}
+  <div class="challenge-card">
+    <img :src="`${props.image}`" :alt="`${props.name}`" class="challenge-img" />
+    <div class="point-box">
+      <img src="/image/main/point.png" alt="point" class="point" />
+      <span class="point-text">{{ `${props.reward}P` }}</span>
     </div>
   </div>
 </template>
 
-<style scoped>
-.point {
-  width: 15px;
+<style lang="scss" scoped>
+.challenge-card {
+  position: relative;
+  width: 168px;
+  height: 121px;
+  border-radius: 10px;
+  box-shadow: 0 3px 5px rgba(0, 0, 0, 0.1);
+  overflow: hidden;
+  // .challenge-img {
+  // }
+  .point-box {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    width: 45px;
+    height: 20px;
+    border-radius: 10px;
+    background-color: #d9d9d9;
+    top: 5px;
+    right: 5px;
+
+    .point {
+      width: 14px;
+    }
+
+    .point-text {
+      font-size: 9px;
+      color: #fafafa;
+    }
+  }
 }
 </style>
